18386027d7fe4b9efaa8ce7edd6fd8f014509cf7
oninsan <kokoybaldofordawin@gmail.com>
2025-03-18 14:50:35 +0800

    Removed the solutions.

--- Easy/Python/sum_of_digits.py
+++
@@ -1,5 +0,0 @@
-def sum_of_digits(n):
-  return sum(int(digit) for digit in str(abs(n)))
-
-num = int(input("Enter a number: "))
-print(sum_of_digits(num))
--- Intermediate/Python/reverse_string.py
+++
@@ -1,8 +0,0 @@
-def reverse_string(s):
-  reversed_str = ""
-  for char in s:
-    reversed_str = char + reversed_str
-  return reversed_str
-
-s = input("Enter a string: ")
-print(reverse_string(s))
